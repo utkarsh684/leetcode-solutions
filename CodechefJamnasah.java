4486d03c4669f12a04f194d0b23481eea62f10e1
utkarsh684 <shuklautkarsh406@gmail.com>
2025-06-05 11:26:00 +0530

    Jamnasah and assignment

---
+++ CodechefJamnasah.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+
+class CodechefJamnasah
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    int X=sc.nextInt();
+		    if(X<=7){
+		        System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
