0f551449a193c0ef698ef87682f30268cc5c4f30
Utkarsh Shukla <shuklautkarsh406@gmail.com>
2025-06-06 19:10:46 +0530

    Create ExamsCodechef.java
    exams of codechef

---
+++ ExamsCodechef.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int X,Y,Z;
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    X=sc.nextInt();
+		    Y=sc.nextInt();
+		    Z=sc.nextInt();
+		    if((double)Z/(X*Y)>0.5){
+		        System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+
+	}
+}
