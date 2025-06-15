841f639699d9e75c8e1a044e93328d885936b6c7
utkarsh684 <shuklautkarsh406@gmail.com>
2025-06-15 10:31:30 +0530

    leetcode 206

---
+++ leetcode206.java
@@ -0,0 +1,22 @@
+public /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class leetcode206 {
+    public ListNode reverseList(ListNode head){
+        ListNode prev=null;
+        while(head!=null){
+           ListNode temp=head.next;
+           head.next=prev;
+           prev=head;
+           head=temp;
+        }
+        return prev;
+    }
+} 
