11be185af60156603c7a070dac242a9b1fc1e343
utkarsh684 <shuklautkarsh406@gmail.com>
2025-06-16 22:09:40 +0530

    leetcode 145

---
+++ leetcode145.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+class leetcode145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+
+        postorder(root,list);
+        return list;
+
+    }
+
+    void postorder(TreeNode root,List<Integer> list){
+        if(root==null){
+            return ;
+        }
+
+        postorder(root.left,list);
+        postorder(root.right,list);
+        list.add(root.val);
+    }
+}
